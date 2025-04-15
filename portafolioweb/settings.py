75d646e1224d53f899b8b8891334ea9fb9d4b73d
Victor29239293 <vcelir2@unemi.edu.ec>
2025-04-15 14:26:31 -0500

    static_root

--- portafolioweb/settings.py
+++ portafolioweb/settings.py
@@ -131,7 +131,8 @@ NPM_BIN_PATH = r'C:\Users\Victor Celi\AppData\Roaming\npm\npm.cmd'
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # <- necesario para Render
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # si usas 'static/' en tu proyecto
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
