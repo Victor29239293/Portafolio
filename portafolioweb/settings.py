c9db574f2e1067f2666387b0da4915390cc02493
Victor29239293 <vcelir2@unemi.edu.ec>
2025-04-14 22:51:43 -0500

    Configuración de base de datos para Render

--- portafolioweb/settings.py
+++ portafolioweb/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-d)%d*yqtou63y&_gtr!+0e#1uy7xe6jpfq@z-qr#xnm0d6uvd$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.114', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 INTERNAL_IPS = [
@@ -81,14 +83,16 @@ WSGI_APPLICATION = 'portafolioweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
